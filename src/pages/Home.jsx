c6e74efd75407a28188eb53d89e854c3242342c3
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-09-24 14:43:39 +0545

    Added Resume Button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,63 +1,75 @@
-import React from "react";
-import Section from "../components/Section";
+import React from 'react'
+import Section from '../components/Section'
+import ResumePDF from '../assets/resume.pdf'
 
 const SocialIcon = ({ href, icon }) => {
   return (
     <a
-      className="social-icon"
+      className='social-icon'
       href={href}
-      target="_blank"
-      rel="noopener noreferrer"
+      target='_blank'
+      rel='noopener noreferrer'
     >
       <i className={`fab ${icon}`}></i>
     </a>
-  );
-};
+  )
+}
 
 export default function Home() {
   const icons = [
     {
-      href: "https://www.linkedin.com/in/aashish-maharjan-788b661b0/",
-      icon: "fa-linkedin-in",
+      href: 'https://www.linkedin.com/in/aashish-maharjan-788b661b0/',
+      icon: 'fa-linkedin-in',
     },
     {
-      href: "https://github.com/aacismaharjan",
-      icon: "fa-github",
+      href: 'https://github.com/aacismaharjan',
+      icon: 'fa-github',
     },
     {
-      href: "https://twitter.com/aacismhrzn",
-      icon: "fa-twitter",
+      href: 'https://twitter.com/aacismhrzn',
+      icon: 'fa-twitter',
     },
     {
-      href: "https://www.facebook.com/aashishmaharjan.2001",
-      icon: "fa-facebook-f",
+      href: 'https://www.facebook.com/aashishmaharjan.2001',
+      icon: 'fa-facebook-f',
     },
-  ];
+  ]
 
   return (
-    <Section id="about">
-      <h1 className="mb-0">
+    <Section id='about'>
+      <h1 className='mb-0'>
         Aashish
-        <span className="text-primary">Maharjan</span>
+        <span className='text-primary'>Maharjan</span>
       </h1>
-      <div className="subheading mb-5">
+      <div className='subheading mb-5'>
         Purano Kalimati, Kathmandu, Nepal ·
-        <a href="mailto:aashishmaharjan2001@gmail.com">
+        <a href='mailto:aashishmaharjan2001@gmail.com'>
           aashishmaharjan2001@gmail.com
         </a>
       </div>
-      <p className="lead mb-5">
+      <p className='lead mb-4'>
         Aashish Maharjan connects online brands to their target audiences for
         the perfect communicating experience. At the Gyanodaya School, Aashish
         learned the importance of applying classic web designing ideas to modern
         brands strategies from experts in the field, including his Computer
         Teacher, Mr. Mannor Shakya.
       </p>
-      <div className="social-icons">
+
+      <div className='btn-group mb-5'>
+        <button
+          className='btn btn-primary btn-custom py-2 px-5'
+          type='submit'
+          onClick={() => window.open(ResumePDF)}
+        >
+          Download CV
+        </button>
+      </div>
+
+      <div className='social-icons'>
         {icons.map(({ href, icon }, index) => (
           <SocialIcon href={href} icon={icon} key={index} />
         ))}
       </div>
     </Section>
-  );
+  )
 }

66673f7ae4897a914bd144b7116f13e9b8a126c6
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-11-12 10:08:49 +0545

    Added loading animation to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Card } from 'react-bootstrap'
+import { motion, AnimatePresence } from 'framer-motion'
+import loadingGif from '../assets/aacismaharjan-loading-logo.svg'
+import { useEffect } from 'react'
 
 export default function PortfolioCard({
   card: { img, title, desc, demo, source },
@@ -8,9 +11,39 @@ export default function PortfolioCard({
     backgroundImage: `url(${img})`,
   }
 
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const onImageLoaded = () => {
+      const tempImg = new Image()
+      tempImg.src = img
+      tempImg.onload = setIsLoading(false)
+    }
+    onImageLoaded()
+  }, [img])
+
   return (
     <Card data-aos='flip-left'>
-      <div className='card-thumbnail' style={thumbnail} />
+      <div className='card-thumbnail' style={thumbnail}>
+        <AnimatePresence>
+          {isLoading && (
+            <motion.div
+              className='overlay'
+              initial={{ scale: 0, opacity: 0 }}
+              animate={{
+                scale: 1,
+                opacity: 1,
+                type: 'spring',
+                stiffness: 2000,
+              }}
+              exit={{ scale: 2, opacity: 0 }}
+              transition={{ duration: 0.25 }}
+            >
+              <img src={loadingGif} alt='Loading aacismaharjan...' />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
       <Card.Body>
         <Card.Title className='title'>{title}</Card.Title>
         <Card.Text>{desc}</Card.Text>

cf9319656ea78e1905db5cb030d565a6643a3b75
aacismaharjan <aashishmaharjan2001@gmail.com>
2021-10-15 00:20:40 +0545

    [Fix] Showing popular portolios only

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { useEffect } from "react"
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./css/App.scss"
-import Navbar from "./components/Navbar"
-import Home from "./pages/Home"
-import Skills from "./pages/Skills"
-import Portfolio from "./pages/Portfolio"
-import { Route, Switch } from "react-router-dom"
-import Codepen from "./pages/Codepen"
-import AOS from "aos"
-import "aos/dist/aos.css"
-import Contact from "./pages/Contact"
-import { Helmet } from "react-helmet"
+import React, { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/App.scss';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Skills from './pages/Skills';
+import Portfolio from './pages/Portfolio';
+import { Route, Switch } from 'react-router-dom';
+import Codepen from './pages/Codepen';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Contact from './pages/Contact';
+import { Helmet } from 'react-helmet';
 
 function App() {
   useEffect(() => {
-    AOS.init()
-  }, [])
+    AOS.init();
+  }, []);
 
   return (
     <Switch>
@@ -24,7 +24,7 @@ function App() {
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1.0"
-          ></meta>
+          />
           <meta
             name="description"
             content="Aashish Maharjan - Frontend React Web Developer who loves javascript and modern web technologies."
@@ -42,7 +42,7 @@ function App() {
         </div>
       </Route>
     </Switch>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/api/portfolio.js
+++ src/api/portfolio.js
@@ -1,133 +1,129 @@
-import CoronaTrackerThumb from "../assets/portfolio/coronatracker.jpg"
-import HamroJourneyThumb from "../assets/portfolio/hamrojourney.jpg"
-import HimalanResortThumb from "../assets/portfolio/himalayanresort.jpg"
-import RMSThumb from "../assets/portfolio/rms.jpg"
-import QuizbeeThumb from "../assets/portfolio/quizbee.jpg"
-import YoutubeCloneThumb from "../assets/portfolio/youtube-clone.jpg"
-import SimplePortfolioThumb from "../assets/portfolio/simple_portfolio.jpg"
-import CoolorsThumb from "../assets/portfolio/coolors.jpg"
-import GetWatchThumb from "../assets/portfolio/getwatch.jpg"
-import ReactReduxRestThumb from "../assets/portfolio_hd/react-redux-rest.png"
-import GithubUserStatThumb from "../assets/portfolio/github_user_stats.jpg"
-import MernTwitterCloneThumb from "../assets/portfolio/mern-twitter-clone.png"
-import LandingPageTemplateThumb from "../assets/portfolio/landing_page_template.jpg"
-import AppLandingPageThumb from "../assets/portfolio/app-landingpage.png"
+import CoronaTrackerThumb from '../assets/portfolio/coronatracker.jpg';
+import HamroJourneyThumb from '../assets/portfolio/hamrojourney.jpg';
+import HimalanResortThumb from '../assets/portfolio/himalayanresort.jpg';
+import RMSThumb from '../assets/portfolio/rms.jpg';
+import QuizbeeThumb from '../assets/portfolio/quizbee.jpg';
+import YoutubeCloneThumb from '../assets/portfolio/youtube-clone.jpg';
+import SimplePortfolioThumb from '../assets/portfolio/simple_portfolio.jpg';
+import CoolorsThumb from '../assets/portfolio/coolors.jpg';
+import GetWatchThumb from '../assets/portfolio/getwatch.jpg';
+import ReactReduxRestThumb from '../assets/portfolio_hd/react-redux-rest.png';
+import GithubUserStatThumb from '../assets/portfolio/github_user_stats.jpg';
+import MernTwitterCloneThumb from '../assets/portfolio/mern-twitter-clone.png';
+import LandingPageTemplateThumb from '../assets/portfolio/landing_page_template.jpg';
+import AppLandingPageThumb from '../assets/portfolio/app-landingpage.png';
+import NetflixCloneThumb from '../assets/portfolio_hd/netflix-clone.png';
 
 export default [
+  // {
+  //   img: MernTwitterCloneThumb,
+  //   title: "Simple Twitter CRUD - MERN Stack",
+  //   desc:
+  //     "Simple CRUD MERN Stack app that allows creating, reading, updating and deleting tweets and basic look of Twitter",
+  //   demo: "https://mern-twitter-crud.herokuapp.com/",
+  //   source: "https://mern-twitter-crud.herokuapp.com/",
+  // },
   {
-    img: MernTwitterCloneThumb,
-    title: "Simple Twitter CRUD - MERN Stack",
-    desc:
-      "Simple CRUD MERN Stack app that allows creating, reading, updating and deleting tweets and basic look of Twitter",
-    demo: "https://mern-twitter-crud.herokuapp.com/",
-    source: "https://mern-twitter-crud.herokuapp.com/",
+    img: NetflixCloneThumb,
+    title: 'Netflix Clone - ReactJS',
+    desc: "Netflix Clone featuring Netflix's homepage and series / films page with authenticatio by the help of Firebase as backend.",
+    demo: 'https://netflix-clone-aacis.netlify.app/',
+    source: 'https://github.com/aacismaharjan/netflix-clone',
   },
-
   {
     img: HimalanResortThumb,
-    title: "Himalayan Resort - ReactJS",
-    desc:
-      "A resort for fictional hotel, in which we can filter out the hotel rooms and see specific info about the hotel.",
-    demo: "https://himalayanresort.netlify.app/",
-    source: "https://github.com/aacismaharjan/himalayanresort",
-  },
-  {
-    img: GithubUserStatThumb,
-    title: "Github User Stats - React.js",
-    desc:
-      "Fetches Github Api of user and processes it into meaningful information with the help of fusion charts and Auth0",
-    demo: "https://github-user-stats.netlify.app/",
-    source: "https://github.com/aacismaharjan/search-github-user",
-  },
-  {
-    img: CoronaTrackerThumb,
-    title: "Corona Tracker - ReactJS",
-    desc:
-      "It shows the overall corona cases throughout the world, Also presents data in readable diagrams/charts.",
-    demo: "https://aacismaharjan.github.io/coronatracker/",
-    source: "https://github.com/aacismaharjan/coronatracker",
-  },
-  {
-    img: ReactReduxRestThumb,
-    title: "Redux Redux Rest - React, Redux & Thunk",
-    desc:
-      "A simple react-redux, redux-thunk web app for create, read, update and delete restful apis.",
-    demo: "https://aacismaharjan.github.io/react-redux-rest/",
-    source: "https://github.com/aacismaharjan/react-redux-rest",
-  },
-  {
-    img: GetWatchThumb,
-    title: "APPLE WATCH - REACTJS TEMPLATE",
-    desc:
-      "Created with highly reusable components, can be updated from remote file.",
-    demo: "https://getwatch.netlify.app/",
-    source: "https://getwatch.netlify.app/",
-  },
-  {
-    img: AppLandingPageThumb,
-    title: "Fusion App landing page - HTML & CSS",
-    desc:
-      "Fusion App HTML Template created using HTML, CSS, Bootstrap, JS, Jquery, and other thrid party libraries.",
-    demo: "https://fusion-app-landing-page.netlify.app/",
-    source: "https://fusion-app-landing-page.netlify.app/",
+    title: 'Himalayan Resort - ReactJS',
+    desc: 'A resort for fictional hotel, in which we can filter out the hotel rooms and see specific info about the hotel.',
+    demo: 'https://himalayanresort.netlify.app/',
+    source: 'https://github.com/aacismaharjan/himalayanresort',
   },
   {
     img: LandingPageTemplateThumb,
-    title: "Landing Page Template - HTML & CSS",
-    desc:
-      "Landing page template for watch product. Created with HTML, CSS, JS, Jquery, Bootstrap and other production ready libraries.",
-    demo: "https://getwatch.netlify.app/",
-    source: "https://github.com/aacismaharjan/react_landing_page",
+    title: 'Landing Page Template - HTML & CSS',
+    desc: 'Landing page template for watch product. Created with HTML, CSS, JS, Jquery, Bootstrap and other production ready libraries.',
+    demo: 'https://getwatch.netlify.app/',
+    source: 'https://github.com/aacismaharjan/react_landing_page',
   },
   {
-    img: YoutubeCloneThumb,
-    title: "Youtube Clone - ReactJS",
-    desc:
-      "Simple Youtube Clone with React.js. It utilizes the Youtube api to fetch the data and display it",
-    demo: "https://aacismaharjan.github.io/youtube-clone/",
-    source: "https://github.com/aacismaharjan/youtube-clone",
+    img: AppLandingPageThumb,
+    title: 'Fusion App landing page - HTML & CSS',
+    desc: 'Fusion App HTML Template created using HTML, CSS, Bootstrap, JS, Jquery, and other thrid party libraries.',
+    demo: 'https://fusion-app-landing-page.netlify.app/',
+    source: 'https://fusion-app-landing-page.netlify.app/',
   },
+  {
+    img: GithubUserStatThumb,
+    title: 'Github User Stats - React.js',
+    desc: 'Fetches Github Api of user and processes it into meaningful information with the help of fusion charts and Auth0',
+    demo: 'https://github-user-stats.netlify.app/',
+    source: 'https://github.com/aacismaharjan/search-github-user',
+  },
+  {
+    img: CoronaTrackerThumb,
+    title: 'Corona Tracker - ReactJS',
+    desc: 'It shows the overall corona cases throughout the world, Also presents data in readable diagrams/charts.',
+    demo: 'https://aacismaharjan.github.io/coronatracker/',
+    source: 'https://github.com/aacismaharjan/coronatracker',
+  },
+  // {
+  //   img: ReactReduxRestThumb,
+  //   title: "Redux Redux Rest - React, Redux & Thunk",
+  //   desc:
+  //     "A simple react-redux, redux-thunk web app for create, read, update and delete restful apis.",
+  //   demo: "https://aacismaharjan.github.io/react-redux-rest/",
+  //   source: "https://github.com/aacismaharjan/react-redux-rest",
+  // },
+  // {
+  //   img: GetWatchThumb,
+  //   title: "APPLE WATCH - REACTJS TEMPLATE",
+  //   desc:
+  //     "Created with highly reusable components, can be updated from remote file.",
+  //   demo: "https://getwatch.netlify.app/",
+  //   source: "https://getwatch.netlify.app/",
+  // },
+
+  // {
+  //   img: YoutubeCloneThumb,
+  //   title: "Youtube Clone - ReactJS",
+  //   desc:
+  //     "Simple Youtube Clone with React.js. It utilizes the Youtube api to fetch the data and display it",
+  //   demo: "https://aacismaharjan.github.io/youtube-clone/",
+  //   source: "https://github.com/aacismaharjan/youtube-clone",
+  // },
   {
     img: SimplePortfolioThumb,
-    title: "Simple Portfolio - HTML Template",
-    desc:
-      "Simple Portfolio Template using Pure HTML, CSS & Javascript. No use of any third party libraries.",
-    demo: "https://aacismaharjan.github.io/simple_portfolio/",
-    source: "https://github.com/aacismaharjan/simple_portfolio",
+    title: 'Simple Portfolio - HTML Template',
+    desc: 'Simple Portfolio Template using Pure HTML, CSS & Javascript. No use of any third party libraries.',
+    demo: 'https://aacismaharjan.github.io/simple_portfolio/',
+    source: 'https://github.com/aacismaharjan/simple_portfolio',
   },
   {
     img: HamroJourneyThumb,
-    title: "Hamro Journey - HTML, CSS & JS",
-    desc:
-      "This is simple HTML Template which I have created and won in SXC Web Design Competition 2019.",
-    demo: "https://aacismaharjan.github.io/hamrojourney/",
-    source: "https://github.com/aacismaharjan/hamrojourney",
+    title: 'Hamro Journey - HTML, CSS & JS',
+    desc: 'This is simple HTML Template which I have created and won in SXC Web Design Competition 2019.',
+    demo: 'https://aacismaharjan.github.io/hamrojourney/',
+    source: 'https://github.com/aacismaharjan/hamrojourney',
   },
-
-  {
-    img: QuizbeeThumb,
-    title: "Quizbee - Reactjs and External Api",
-    desc:
-      "Simple Quizbee web app build with ReactJS which eats the api and interacts with users!",
-    demo: "https://aacismaharjan.github.io/quizbee/",
-    source: "https://github.com/aacismaharjan/quizbee",
-  },
+  // {
+  //   img: QuizbeeThumb,
+  //   title: "Quizbee - Reactjs and External Api",
+  //   desc:
+  //     "Simple Quizbee web app build with ReactJS which eats the api and interacts with users!",
+  //   demo: "https://aacismaharjan.github.io/quizbee/",
+  //   source: "https://github.com/aacismaharjan/quizbee",
+  // },
   {
     img: CoolorsThumb,
-    title: "Coolors Everywhere! - ReactJS",
-    desc:
-      "Coolors website for fullfiling every developers color need! Find colors, complementary colors, html colors & much more.",
-    demo: "https://aacismaharjan.github.io/coolors/",
-    source: "https://github.com/aacismaharjan/coolors",
+    title: 'Coolors Everywhere! - ReactJS',
+    desc: 'Coolors website for fullfiling every developers color need! Find colors, complementary colors, html colors & much more.',
+    demo: 'https://aacismaharjan.github.io/coolors/',
+    source: 'https://github.com/aacismaharjan/coolors',
   },
-
   {
     img: RMSThumb,
-    title: "Result Management System - C Programming",
-    desc:
-      "My first C Programming project in +2. It has CRUD operation only with File handling and has ton of features.",
-    demo:
-      "https://github.com/aacismaharjan/RMS/tree/master/Result%20Managaement%20System%20-%20Version%202.1.0%20-%20Compiled",
-    source: "https://github.com/aacismaharjan/RMS",
+    title: 'Result Management System - C Programming',
+    desc: 'My first C Programming project in +2. It has CRUD operation only with File handling and has ton of features.',
+    demo: 'https://github.com/aacismaharjan/RMS/tree/master/Result%20Managaement%20System%20-%20Version%202.1.0%20-%20Compiled',
+    source: 'https://github.com/aacismaharjan/RMS',
   },
-]
+];

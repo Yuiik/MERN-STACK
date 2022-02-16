39aa92b5eaade36376a9fb5ae0b33c246ffa898b
aacismaharjan <aashishmaharjan2001@gmail.com>
2022-02-16 20:54:04 +0545

    [Fix] Changed sass to css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/App.scss';
+import './css/App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Skills from './pages/Skills';
